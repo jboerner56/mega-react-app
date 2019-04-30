e88652f8101511104fe2d8308de7f5a771eba5c2
Jason Boerner <jboerner56@gamil.com>
2019-04-30 08:01:08 -0400

    setup and weather.js file

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Weather (props) {
+    return(
+        <ul>
+            <li>Temp:</li>
+            <li></li>
+        </ul>
+    )
+}
+
+
+export default Weather;
